c767261228baaa1eeb152aa48e6d77195c9b6e1a
mingwiki <mingwiki@gmail.com>
2023-03-13 18:53:39 +0800

    update

--- alias.zsh
+++ alias.zsh
@@ -1,12 +1,12 @@
-alias sp='export all_proxy=socks5://10.10.10.10:10800'
+alias sp='export all_proxy=socks5://10.10.10.10:20000'
 alias unsp='unset all_proxy'
-alias ytc='yt-dlp --proxy socks5://10.10.10.10:10800 --cookies=/root/cookie.txt --verbose --no-check-certificates'
-alias yt='yt-dlp --proxy socks5://10.10.10.10:10800 --verbose --no-check-certificates'
+alias yt='hup yt-dlp --proxy socks5://10.10.10.10:20000 --verbose --no-check-certificates'
+alias ytc='yt --cookies=/root/cookie.txt'
 alias cl='clear'
 alias gl='glances'
-alias nsync='rsynclog'
-alias cat='batcat'
-# Function to run rsync with logging
-rsynclog() {
-  nohup rsync "$@" > ~/rsync.log 2>&1 &
+alias nsync='hup rsync'
+alias sz='source ~/.zshrc'
+hup() {
+  nohup "$@" &>>"$1".log &
+  tail -F "$1".log
 }
