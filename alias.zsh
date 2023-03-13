c99ba0e2d436f178e3987e04c66faad96d293e0c
mingwiki <mingwiki@gmail.com>
2023-03-13 23:27:30 +0800

    update

--- alias.zsh
+++ alias.zsh
@@ -4,10 +4,15 @@ alias yt='hup yt-dlp --proxy socks5://10.10.10.10:20000 --verbose --no-check-cer
 alias ytc='yt --cookies=/root/cookie.txt'
 alias cl='clear'
 alias gl='glances'
-alias nsync='hup rsync'
+alias nsync='huphome rsync'
 alias sz='source ~/.zshrc'
 alias mv='mv -iuv'
+alias wt='watch tail -n 20'
 hup() {
   nohup "$@" &>>"$1".log &
   tail -F "$1".log
+}
+huphome() {
+  nohup "$@" &>>$HOME/"$1".log &
+  tail -F $HOME/"$1".log
 }
