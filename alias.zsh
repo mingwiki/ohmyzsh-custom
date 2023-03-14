2a56b4c29a5a83c12cb07b8cef5ba66566c5aff8
mingwiki <mingwiki@gmail.com>
2023-03-14 10:11:01 +0800

    update

--- alias.zsh
+++ alias.zsh
@@ -1,18 +1,17 @@
 alias sp='export all_proxy=socks5://10.10.10.10:20000'
 alias unsp='unset all_proxy'
-alias yt='hup yt-dlp --proxy socks5://10.10.10.10:20000 --verbose --no-check-certificates'
-alias ytc='yt --cookies=/root/cookie.txt'
 alias cl='clear'
 alias gl='glances'
-alias nsync='huphome rsync'
+alias nsync='hup2home rsync'
 alias sz='source ~/.zshrc'
 alias mv='mv -iuv'
+alias yt='hup yt-dlp --proxy socks5://10.10.10.10:20000 --verbose --no-check-certificates'
 alias wt='watch tail -n 20'
 hup() {
   nohup "$@" &>>"$1".log &
   tail -F "$1".log
 }
-huphome() {
+hup2home() {
   nohup "$@" &>>$HOME/"$1".log &
   tail -F $HOME/"$1".log
 }
